76a6b9d9ea0462cadff966985ca947a568077221
sachinOraon <sachinoraon55@gmail.com>
2020-05-14 16:58:30 +0530

    wait for some time for link with very few seeds

--- leecher.py
+++ leecher.py
@@ -14,15 +14,21 @@ params = {
 
 link = sys.argv[1]
 logfile = sys.argv[2]
+WAIT_CNT = 0
 f = open(logfile, 'w+')
 handle = lt.add_magnet_uri(ses, link, params)
 ses.start_dht()
 f.write('[*] Getting file info\n')
 while (not handle.has_metadata()):
+    WAIT_CNT += 1
     time.sleep(1)
+    if WAIT_CNT == 120: # wait for 2mins
+       f.write('[*] '+link+'\n[*] Unable to get file info due to very few seeds\n[*] Process Terminated')
+       f.close()
+       sys.exit()
 f.write('[*] '+handle.status().name+'\n')
 while (handle.status().state != lt.torrent_status.seeding):
-    time.sleep(5)
+    time.sleep(10)
     s = handle.status()
     f.write('[*] %6.2f%% [Speed: %7.1fKB/s|Seeds:%3d|Peers:%3d] [%s]\n' % (s.progress * 100, s.download_rate / 1024, s.num_seeds, s.num_peers, s.state))
 f.write('[*] Completed')
