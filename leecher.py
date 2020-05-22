6c31efcadc7bbcd93464b59af8d4d9c5ae681efc
sachinOraon <sachinoraon55@gmail.com>
2020-05-22 13:52:34 +0530

    added timestamp in log file

--- leecher.py
+++ leecher.py
@@ -1,6 +1,7 @@
 import libtorrent as lt
 import time
 import sys
+from datetime import datetime
 
 ses = lt.session()
 ses.listen_on(6881, 6891)
@@ -18,11 +19,14 @@ WAIT_CNT = 0
 f = open(logfile, 'w+')
 handle = lt.add_magnet_uri(ses, link, params)
 ses.start_dht()
+start=datetime.now()
+start_time=start.strftime("%d/%m/%Y %H:%M:%S")
+f.write('[*] '+start_time+'\n')
 f.write('[*] Getting file info\n')
 while (not handle.has_metadata()):
     WAIT_CNT += 1
     time.sleep(1)
-    if WAIT_CNT == 120: # wait for 2mins
+    if WAIT_CNT == 180: # wait for 3mins
        f.write('[*] '+link+'\n[*] Unable to get file info due to very few seeds\n[*] Process Terminated')
        f.close()
        sys.exit()
@@ -31,6 +35,9 @@ while (handle.status().state != lt.torrent_status.seeding):
     time.sleep(10)
     s = handle.status()
     f.write('[*] %6.2f%% [Speed: %7.1fKB/s|Seeds:%3d|Peers:%3d] [%s]\n' % (s.progress * 100, s.download_rate / 1024, s.num_seeds, s.num_peers, s.state))
-f.write('[*] Completed')
+end=datetime.now()
+f.write('[*] Completed\n')
+end_time=end.strftime("%d/%m/%Y %H:%M:%S")
+f.write('[*] '+end_time)
 f.close()
 sys.exit()
